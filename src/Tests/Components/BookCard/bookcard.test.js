9cbadbaebb011de2f90a92397408eb8419868c01
Abhinav Kumar <kumarabhinav75@gmail.com>
2019-02-18 13:30:23 +0530

    Added test for bookcard

--- src/Tests/Components/BookCard/bookcard.test.js
+++ src/Tests/Components/BookCard/bookcard.test.js
@@ -12,4 +12,14 @@ describe('BookCard', () => {
     const tree = renderer.create(<BookCard bookData = {book_data} />);
     expect(tree).toMatchSnapshot();
   })
+
+  it('Should toggle inverse like', ()=> {
+    const book_data = {
+      Name: "Abhinav",
+      rating: "4.5"
+    }
+    const wrapper = shallow(<BookCard bookData = {book_data} liked={true} />);
+    wrapper.instance().toggleLike();
+    expect(wrapper.instance().state.liked).toEqual(false);
+  });
 })
